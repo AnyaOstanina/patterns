1ce3e9a1db09e7ab3094fc25d1f3ecbdfda00685
Anya Ostanina <oav@paraweb.me>
2020-01-11 17:21:46 +0700

    refactor player controller Feature Envy search team by id

--- src/main/java/com/electricalweb/controllers/PlayerController.java
+++ src/main/java/com/electricalweb/controllers/PlayerController.java
@@ -1,15 +1,11 @@
 package com.electricalweb.controllers;
 import com.electricalweb.entities.Player;
 import com.electricalweb.entities.Team;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @WebServlet(name = "PlayerController", urlPatterns = "/player")
 public class PlayerController extends HttpServlet {
@@ -33,7 +29,7 @@ public class PlayerController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         long idTeam = Integer.valueOf(req.getParameter("idTeam"));
-        Team team = searchTeamById(idTeam);
+        Team team = playerService.searchTeamById(idTeam);
         List<Player> players=team.getPlayers();
         setAttributes(req, idTeam,players);
         String url = playerService.determineUrl();
@@ -49,21 +45,11 @@ public class PlayerController extends HttpServlet {
             throws Exception {
         String name = req.getParameter("playerName");
         long idTeam = Integer.valueOf(req.getParameter("teamId"));
-        Team team = searchTeamById(idTeam);
+        Team team = playerService.searchTeamById(idTeam);
         Player player = new Player(name,team.getName());
         List<Player> players=playerService.addPlayer(player, idTeam);
         setAttributes(req, idTeam, players);
         String url = playerService.determineUrl();
         playerService.forwardResponse(url, req, resp);
-    }
-
-    private Team searchTeamById(long id) {
-        Team team = null;
-        try {
-            team = (Team) playerService.teamList.searchEntityById(id);
-        } catch (Exception ex) {
-            Logger.getLogger(GameController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return team;
     }
 }
--- src/main/java/com/electricalweb/controllers/PlayerService.java
+++ src/main/java/com/electricalweb/controllers/PlayerService.java
@@ -2,6 +2,8 @@ package com.electricalweb.controllers;
 import com.electricalweb.entities.*;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PlayerService extends Service {
     public PlayerService() {
@@ -15,5 +17,15 @@ public class PlayerService extends Service {
         Team team = (Team) teamList.searchEntityById(id);
         team.setPlayer(player);
         return team.getPlayers();
+    }
+
+    public Team searchTeamById(long id) {
+        Team team = null;
+        try {
+            team = (Team) teamList.searchEntityById(id);
+        } catch (Exception ex) {
+            Logger.getLogger(GameController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return team;
     }
 }
